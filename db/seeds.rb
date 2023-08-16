4daa7a4ce1b0801f5a91bdba784d19ca42639577
Jerodenni <jeromedenni@hotmail.fr>
2023-08-16 16:22:18 +0200

    finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Restaurant.destroy_all
+
+puts 'Creating 5 fake restaurants...'
+5.times do
+    restaurant = Restaurant.new(
+      name:    Faker::Beer.name,
+      address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+      phone_number: Faker::PhoneNumber.phone_number,
+      category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    )
+    restaurant.save!
+    end
+puts 'Finished!'
